f25cfe7b8a0556876375f99d68d2fceac067f7d5
DamocValentin <valentin.damoc@yahoo.com>
2019-12-26 10:23:02 +0200

    Added Blog post views

--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -77,7 +77,7 @@ class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     email = fields.Email(required=True)
-    password = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
     created_at = fields.DateTime(dump_only=True)
     modified_at = fields.DateTime(dump_only=True)
     blogposts = fields.Nested(BlogpostSchema, many=True)
