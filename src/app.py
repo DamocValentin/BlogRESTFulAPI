3369890fc117fc1733be2f830607bd8ba2a9d01c
DamocValentin <valentin.damoc@yahoo.com>
2019-12-26 10:45:49 +0200

    Added testing

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ from .models import db, bcrypt
 
 # import user_api blueprint
 from .views.UserView import user_api as user_blueprint
+from .views.BlogpostView import blogpost_api as blogpost_blueprint
 
 
 def create_app(env_name):
@@ -20,10 +21,10 @@ def create_app(env_name):
 
     # initializing bcrypt
     bcrypt.init_app(app)
-
     db.init_app(app)
 
     app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+    app.register_blueprint(blogpost_blueprint, url_prefix='/api/v1/blogposts')
 
     @app.route('/', methods=['GET'])
     def index():
